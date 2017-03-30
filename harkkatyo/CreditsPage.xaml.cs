79ad499678bac74dc098fe8585d55c6eb0dfca76
taru <k8230@student.jamk.fi>
2017-03-30 14:32:31 +0300

    alkurunkoa

--- harkkatyo/CreditsPage.xaml.cs
+++ harkkatyo/CreditsPage.xaml.cs
@@ -13,29 +13,38 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace harkkatyo
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class CreditsPage : Page
     {
-        public MainPage()
+        public CreditsPage()
         {
             this.InitializeComponent();
         }
 
-        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(GamePage1));
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null) return;
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
         }
 
-        private void CreditsButton_Click(object sender, RoutedEventArgs e)
+        private void TaruButton_Click(object sender, RoutedEventArgs e)
         {
-            //siirry tekijat sivulle
-            this.Frame.Navigate(typeof(CreditsPage));
+     
+        }
+
+        private void SuviButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
